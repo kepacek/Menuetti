089c2fa2585a47e28193ec3718448fb4886f4c1e
lauramalinen <laura.malinen@gmail.com>
2018-11-20 11:48:13 +0200

    Add project files.

---
+++ Menuetti/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Menuetti.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
