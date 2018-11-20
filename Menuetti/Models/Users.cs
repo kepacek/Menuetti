a7166a265147df64fc37d11060a959a1604626d7
lauramalinen <laura.malinen@gmail.com>
2018-11-20 14:58:21 +0200

    projekti alustettu

---
+++ Menuetti/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Menuetti.Models
+{
+    public partial class Users
+    {
+        public Users()
+        {
+            MyLists = new HashSet<MyLists>();
+            Recipes = new HashSet<Recipes>();
+        }
+
+        public string UserId { get; set; }
+
+        public virtual ICollection<MyLists> MyLists { get; set; }
+        public virtual ICollection<Recipes> Recipes { get; set; }
+    }
+}
