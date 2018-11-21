ef79f2ab35960b4cf95847aff70d405a343ccee6
kepacek <katrie.pacek@gmail.com>
2018-11-21 14:27:08 +0200

    authentication added

--- Menuetti/Controllers/HomeController.cs
+++ Menuetti/Controllers/HomeController.cs
@@ -5,13 +5,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Menuetti.Models;
+using Microsoft.AspNetCore.Authentication;
+using System.Globalization;
 
 namespace Menuetti.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            // If the user is authenticated, then this is how you can get the access_token and id_token
+            if (User.Identity.IsAuthenticated)
+            {
+                string accessToken = await HttpContext.GetTokenAsync("access_token");
+
+                // if you need to check the access token expiration time, use this value
+                // provided on the authorization response and stored.
+                // do not attempt to inspect/decode the access token
+                DateTime accessTokenExpiresAt = DateTime.Parse(
+                    await HttpContext.GetTokenAsync("expires_at"),
+                    CultureInfo.InvariantCulture,
+                    DateTimeStyles.RoundtripKind);
+
+                string idToken = await HttpContext.GetTokenAsync("id_token");
+
+                // Now you can use them. For more info on when and how to use the 
+                // access_token and id_token, see https://auth0.com/docs/tokens
+            }
+
             return View();
         }
 
--- Menuetti/Startup.cs
+++ Menuetti/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Menuetti.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -32,6 +34,71 @@ namespace Menuetti
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+    .AddCookie()
+    .AddOpenIdConnect("Auth0", options =>
+    {
+        // Set the authority to your Auth0 domain
+        options.Authority = $"https://{Configuration["Auth0:Domain"]}";
+
+        // Configure the Auth0 Client ID and Client Secret
+        options.ClientId = Configuration["Auth0:ClientId"];
+        options.ClientSecret = Configuration["Auth0:ClientSecret"];
+
+        // Set response type to code
+        options.ResponseType = "code";
+
+        // Configure the scope
+        options.Scope.Clear();
+        options.Scope.Add("openid");
+
+        // Set the callback path, so Auth0 will call back to http://localhost:5000/signin-auth0
+        // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
+        options.CallbackPath = new PathString("/signin-auth0");
+
+        // Configure the Claims Issuer to be Auth0
+        options.ClaimsIssuer = "Auth0";
+
+        // Saves tokens to the AuthenticationProperties
+        options.SaveTokens = true;
+
+        options.Events = new OpenIdConnectEvents
+        {
+            //OnRedirectToIdentityProvider = context =>
+            //{
+            //    context.ProtocolMessage.SetParameter("audience", "https://menuetti-appi.eu.auth0.com/api/v2/");
+
+            //    return Task.FromResult(0);
+            //},
+            // handle the logout redirection
+            OnRedirectToIdentityProviderForSignOut = (context) =>
+            {
+                var logoutUri = $"https://{Configuration["Auth0:Domain"]}/v2/logout?client_id={Configuration["Auth0:ClientId"]}";
+
+                var postLogoutUri = context.Properties.RedirectUri;
+                if (!string.IsNullOrEmpty(postLogoutUri))
+                {
+                    if (postLogoutUri.StartsWith("/"))
+                    {
+                        // transform to absolute
+                        var request = context.Request;
+                        postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                    }
+                    logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
+                }
+
+                context.Response.Redirect(logoutUri);
+                context.HandleResponse();
+
+                return Task.CompletedTask;
+            }
+        };
+    });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
@@ -49,10 +116,14 @@ namespace Menuetti
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
@@ -62,6 +133,6 @@ namespace Menuetti
             });
         }
 
-       
+
     }
 }
