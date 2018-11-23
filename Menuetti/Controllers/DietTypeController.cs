9f3bd5f0e2385d223b7792920b3c9dba59de5b3b
lauramalinen <laura.malinen@gmail.com>
2018-11-23 17:26:24 +0200

    initial carousel

---
+++ Menuetti/Controllers/DietTypeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Menuetti.Controllers
+{
+    public class DietTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // diettype/omni
+        public IActionResult Omni()
+        {
+            List<int> recipes = new List<int>();
+            Random rnd = new Random((int)DateTime.Now.Ticks);
+            for (int i = 0; i < 5; i++)
+            {
+                recipes.Add(rnd.Next(1, 10));
+            }
+            return View(recipes);
+        }
+    }
+}
