a442269927fffee2d096c16e35c2079753280b80
marjajohannaK <marjajkurkela@gmail.com>
2018-11-26 09:41:22 +0200

    intermediate carousel

--- Menuetti/Controllers/DietTypeController.cs
+++ Menuetti/Controllers/DietTypeController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Menuetti.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Menuetti.Controllers
 {
@@ -14,15 +16,41 @@ namespace Menuetti.Controllers
         }
 
         // diettype/omni
-        public IActionResult Omni()
+        //public IActionResult Omni()
+        //{
+        //    List<int> recipes = new List<int>();
+        //    Random rnd = new Random((int)DateTime.Now.Ticks);
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        recipes.Add(rnd.Next(1, 10));
+        //    }
+        //    return View(recipes);
+        //}
+
+
+        private readonly MenuettiDBContext _context;
+
+
+        public DietTypeController(MenuettiDBContext context)
         {
-            List<int> recipes = new List<int>();
-            Random rnd = new Random((int)DateTime.Now.Ticks);
-            for (int i = 0; i < 5; i++)
-            {
-                recipes.Add(rnd.Next(1, 10));
-            }
-            return View(recipes);
+            _context = context;
         }
-    }
+
+        public async Task<IActionResult> Omni()
+        {
+            var recipes = await _context.Recipes.ToListAsync();
+            
+            List<string> recipeList = new List<string>();
+            Random rnd = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                int index = rnd.Next(recipes.Count);
+                recipeList.Add(recipes[index].RecipeName);
+            }
+
+            return View(recipeList);
+        }
+
+}
 }
