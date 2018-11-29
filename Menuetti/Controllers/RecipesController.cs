e8f6bcb6c96fc09d5aded5799ede5a7cb3fed9d1
kepacek <katrie.pacek@gmail.com>
2018-11-29 14:19:14 +0200

    login requirements added to recipe and ingredients controllers

--- Menuetti/Controllers/RecipesController.cs
+++ Menuetti/Controllers/RecipesController.cs
@@ -136,16 +136,16 @@ namespace Menuetti.Controllers
             {
                 return NotFound();
             }
-
+            string UserId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value;
             var recipes = await _context.Recipes
                 .Include(r => r.User)
                 .Include(r => r.Ingredients)
-                .FirstOrDefaultAsync(m => m.RecipeId == id);
+                .FirstOrDefaultAsync(m => m.RecipeId == id && m.UserId == UserId);
             if (recipes == null)
             {
                 return NotFound();
             }
-            string UserId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value;
+            
             ViewBag.UserId = UserId;
 
             ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", recipes.UserId);
@@ -195,10 +195,10 @@ namespace Menuetti.Controllers
             {
                 return NotFound();
             }
-
+            string UserId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value;
             var recipes = await _context.Recipes
                 .Include(r => r.User)
-                .FirstOrDefaultAsync(m => m.RecipeId == id);
+                .FirstOrDefaultAsync(m => m.RecipeId == id && m.UserId == UserId);
             if (recipes == null)
             {
                 return NotFound();
