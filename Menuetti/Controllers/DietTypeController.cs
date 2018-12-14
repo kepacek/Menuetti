9e985e2af71ac0efc03be5c83564913bab2a01f5
esle <essi.lehtola@hotmail.com>
2018-12-14 17:38:26 +0200

    LinQ so that u dont get the whole db + swipeable carousel (hard coded)

--- Menuetti/Controllers/DietTypeController.cs
+++ Menuetti/Controllers/DietTypeController.cs
@@ -46,50 +46,60 @@ namespace Menuetti.Controllers
                 recipeList[n] = value;
             }
 
-                //Random rnd = new Random();
+            //Random rnd = new Random();
 
-                //foreach (var item in recipes)
-                //{
-                //    int index = rnd.Next(recipes.Count);
-                //    recipeList.Add(recipes[index]);
-                //}
-                //for (int i = 0; i < 100; i++)
-                //{
-                //    int index = rnd.Next(recipes.Count);
-                //    recipeList.Add(recipes[index].RecipeName);
-                //}
+            //foreach (var item in recipes)
+            //{
+            //    int index = rnd.Next(recipes.Count);
+            //    recipeList.Add(recipes[index]);
+            //}
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    int index = rnd.Next(recipes.Count);
+            //    recipeList.Add(recipes[index].RecipeName);
+            //}
 
 
-                return View(recipeList);
+            return View(recipeList);
         }
 
 
-        // diettype/SekaruokaKaruselli
-        public async Task<IActionResult> SekaruokaKaruselli(int id = 2)
+        // diettype/SekaruokaKaruselli/3
+        public async Task<IActionResult> SekaruokaKaruselli(int id = 3)
         {
-            var recipes = await _context.Recipes
-                            .Include(r => r.Ingredients)
-                            .ToListAsync();
+            // 3 recipes per slider
+            int amountOfRecipesNeeded = id * 3;
 
-            List<Recipes> recipeList = new List<Recipes>();
 
-            foreach (var item in recipes)
+            // get random amount of recipes + their ingredients from the db
+            var recipeAmountInDb = _context.Recipes.Count();
+            var skipNumbers = Enumerable.Range(1, recipeAmountInDb - 1);
+            Random rnd = new Random();
+            var skipNumbersRandomised = skipNumbers.OrderBy(x => rnd.Next()).Take(amountOfRecipesNeeded);
+
+            List<Recipes> recipesAndIncredients = new List<Recipes>();
+
+            foreach (var randomNumber in skipNumbersRandomised)
             {
-                recipeList.Add(item);
-            }
-            Random rng = new Random();
-            int n = recipeList.Count;
-            while (n > 1)
-            {
-                n--;
-                int k = rng.Next(n + 1);
-                Recipes value = recipeList[k];
-                recipeList[k] = recipeList[n];
-                recipeList[n] = value;
-            }
+                var recipe = await _context.Recipes
+                    .Skip(randomNumber)
+                    .Take(1)
+                    .Include(r => r.Ingredients)
+                    .FirstOrDefaultAsync();
+
+                recipesAndIncredients.Add(recipe);
+            } // Could this be done faster?
+
+            //var order = await _context.Recipes
+            //    .Include(r => r.Ingredients)
+            //    .Select(recipe => new { recipe, recipe.Ingredients })
+            //    .OrderBy(x => rnd.Next())
+            //    .Take(amountOfRecipes)
+            //    .ToListAsync();          
+            // This does not work because the view needs a List<Recipe> and this is an anonymous list...
 
             ViewBag.amountOfRecipes = id;
-            return View(recipeList);
+            return View(recipesAndIncredients);
         }
 
         // diettype/vegetarian
